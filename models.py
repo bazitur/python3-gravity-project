f48d231ae3524d6a2e86250f37ccd2833002e471
Bazil Miturich <vasia-miturich@yandex.ru>
2017-04-04 21:04:48 +0300

    Writing models.

--- models.py
+++ models.py
@@ -3,8 +3,16 @@
 
 import numpy as np
 import CONSTANTS as CONST
+from scipy.spatial.distance import euclidean
 
-Dot = np.array
+class Dot(list):
+    def __init__(self, *args):
+        super().__init__(*args)
+    
+    def dist(self, other):
+        return euclidean(self, other)
+
+vectorize = lambda x, y: np.array((x, y))
 
 class Field:
     def __init__(self, dimensions, *initial_objects):
@@ -26,26 +34,30 @@ class Field:
 
 
 class Object:
-    
-    def __init__(self, mass, coords=(0, 0), vector=(0, 0), id=-1):
+    """
+    Abstract Object class.
+    """
+    def __init__(self, mass, coords=(0, 0), acceleration=(0, 0), id=-1):
         self.mass = mass
         self.center = Dot(coords)
-        self.vector = Dot(vector)
+        self.acceleration = Dot(acceleration)
         self.id = id
     
     def calc(self, objects):
         
         for obj in objects:
             if obj is not self:
-                # do the calculus
-                pass
+                distance = self.center.dist(obj.center)
+                force = CONST.G * self.mass * obj.mass / distance**2
+                #unit_vector = 
         
     def commit(self):
         pass
     
     def __repr__(self):
         return "<Object #{id} at ({0:.2f}; {1:.2f})>".format(*self.center, id=self.id)
-    
+
+
 if __name__ == "__main__":
     a = Object(1, vector=(1, 0), id=1)
     b = Object(10, coords=(20, 20), id=2)
